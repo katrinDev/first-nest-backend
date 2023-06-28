a057631aaea8ae56d8e14c467f79ea4cbe81c334
katrinDev <polozkovak8@gmail.com>
2023-06-28 20:03:54 +0300

    postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "Katrin_00",
+      database: "nest-first",
+      models: [],
+      autoLoadModels: true, //ask to create tables out of our models
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable() //cuz we need to inject this service into controller
-export class AppService {
-  getUsers() {
-    return [{ id: 1, name: "Kate" }];
-  }
-}
