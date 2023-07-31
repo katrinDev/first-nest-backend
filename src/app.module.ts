19a496db6ea7b15f55f0f0c2468d0e997e3b6084
katrinDev <polozkovak8@gmail.com>
2023-07-31 15:43:37 +0300

    get static from browser

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,9 @@ import { UserRole } from "./roles/user-role.model";
 import { AuthModule } from "./auth/auth.module";
 import { PostsModule } from "./posts/posts.module";
 import { Post } from "./posts/posts.model";
-import { FilesModule } from './files/files.module';
+import { FilesModule } from "./files/files.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
 
 @Module({
   controllers: [],
@@ -17,6 +19,9 @@ import { FilesModule } from './files/files.module';
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, "static"),
     }),
     SequelizeModule.forRoot({
       dialect: "postgres",
